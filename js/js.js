5a673525293d612ab9af1c351578ac54624efa53
CARLOS MOGOLLON ESPINOZA <mogollon.23carlos05@gmail.com>
2021-04-16 16:06:52 -0500

    Update js.js
    Actualización de la ruta JSON

--- js/js.js
+++ js/js.js
@@ -16,7 +16,7 @@ items.addEventListener('click', e => { btnAumentarDisminuir(e) });
 
 // Traer productos
 const fetchData = async () => {
-    const res = await fetch('./json/ProductosJSON.json');
+    const res = await fetch('Json/ProductosJSON.json');
     const data = await res.json();
     // console.log(data)
     pintarCards(data);
